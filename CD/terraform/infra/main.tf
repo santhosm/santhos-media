a83fb9b7a11bc9884bc50ba24cadc1ef5490c6bb
Santhos Manickam <santhos.manickam@genesys.com>
2022-09-23 08:42:29 +0530

    removed client secret var

--- CD/terraform/infra/main.tf
+++ CD/terraform/infra/main.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    subscription_id           = "348b8c4a-d544-4da4-b252-7d5b46589b99"
     resource_group_name       = "terraform"
     storage_account_name      = "terraformsanthos"
     container_name            = "terraform-state"
